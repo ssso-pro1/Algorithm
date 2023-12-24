e19c0d05cc143c4fedf247d318ee14dce930c62f
Soyeon Jang <77109766+ssso-pro1@users.noreply.github.com>
2023-12-24 17:37:00 +0900

    Update 순서바꾸기.js

--- programmers/0단계/순서바꾸기.js
+++ programmers/0단계/순서바꾸기.js
@@ -1,3 +1,8 @@
 function solution(num_list, n) {
     return num_list.slice(n).concat(num_list.slice(0, n));
 }
+// 다른 풀이
+function solution(num_list, n) {
+    num_list.push(...num_list.splice(0, n));
+    return num_list;
+}
