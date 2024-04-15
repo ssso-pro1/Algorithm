d2f0809906f2d626dd6ddc84f392c2963f1dc5e4
Soyeon Jang <77109766+ssso-pro1@users.noreply.github.com>
2024-04-15 20:42:30 +0900

    Create 1로만들기.js

---
+++ programmers/0단계/1로만들기.js
@@ -0,0 +1,37 @@
+function solution(num_list) {
+    let answer = 0;
+    
+    for (let v of num_list) {
+        while ( v !== 1 ) {
+            switch (v % 2) {
+                case 0:
+                    v = v / 2;
+                    answer += 1;
+                    break;
+                case 1:
+                    v = ( v - 1 ) / 2;
+                    answer += 1;
+                    break;
+            }
+        }
+    }
+    
+    return answer;
+}
+
+// function solution(num_list) {
+//     let answer = 0;
+    
+//     num_list.forEach(v => {
+//         while ( v !== 1 ) {
+//             if ( v % 2 === 0 ) {
+//                 v = v / 2;
+//                 answer += 1;
+//             } else {
+//                 v = ( v - 1 ) / 2;
+//                 answer += 1;
+//             }
+//         }
+//     })
+//     return answer;
+// }
