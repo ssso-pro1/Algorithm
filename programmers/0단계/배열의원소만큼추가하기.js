6f1dcdc6d83bf5bbb7f99eaf2f509fa601b2c954
Soyeon Jang <77109766+ssso-pro1@users.noreply.github.com>
2024-04-23 20:35:57 +0900

    Create 배열의원소만큼추가하기.js

---
+++ programmers/0단계/배열의원소만큼추가하기.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    let answer = [];
+    for (let i=0; i<arr.length; i++) {
+        for (let j=0; j<arr[i]; j++) {
+            answer.push(arr[i]);
+        }
+    }
+    
+    return answer;
+}
+
+    // return arr.reduce((list, num) => [...list, ...new Array(num).fill(num)], []);
